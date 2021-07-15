b83bf574a50251db87c3ff5f4703f4583444b902
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2021-07-15 10:54:11 +0200

    added config, constants and certificates

---
+++ oktoberfest/constants.py
@@ -0,0 +1,6 @@
+PROSIT_SERVER = "proteomicsdb.org:8500"
+CERTIFICATES = {
+    "CA": "certificates/Proteomicsdb-Prosit-v2.crt"
+    "USER": "oktoberfest-production.crt"
+    "KEY": "oktoberfest-production.key"
+}
