e42a0c7b9531a1a8d9bea48c7f0ae2e3e300ca90
WassimG <wassim.gabriel@gmail.com>
2021-10-18 11:18:37 +0200

    Change the way msms split works to work directly with the prosit format

--- oktoberfest/re_score.py
+++ oktoberfest/re_score.py
@@ -5,19 +5,15 @@ import subprocess
 
 import numpy as np
 import pandas as pd
+import tracemalloc
+
 
 from .calculate_features import CalculateFeatures
 
 logger = logging.getLogger(__name__)
 
 
-def calculate_features_single(raw_file_path, split_msms_path, percolator_input_path):
-    logger.info(f"Calculating features for {raw_file_path}")
-    features = CalculateFeatures(search_path = "",
-                      raw_path = raw_file_path)
-    df_search = pd.read_csv(split_msms_path, delimiter = '\t')
-    features.predict_with_aligned_ce(df_search)
-    features.gen_perc_metrics(percolator_input_path)
+
 
 
 class ReScore(CalculateFeatures):
@@ -30,7 +26,16 @@ class ReScore(CalculateFeatures):
     """
     raw_path: str
     raw_files: List[str]
-    
+    features: CalculateFeatures
+
+    def calculate_features_single(self, raw_file_path, split_msms_path, percolator_input_path):
+        logger.info(f"Calculating features for {raw_file_path}")
+        self.features = CalculateFeatures(search_path="",
+                                     raw_path=raw_file_path)
+        df_search = pd.read_csv(split_msms_path, delimiter='\t')
+        self.features.predict_with_aligned_ce(df_search)
+        self.features.gen_perc_metrics(percolator_input_path)
+
     def get_raw_files(self):
         self.raw_files = []
         if os.path.isfile(self.raw_path):
@@ -42,7 +47,7 @@ class ReScore(CalculateFeatures):
             logger.info(f"Found {len(self.raw_files)} raw files in the search directory")
     
     def _get_split_msms_path(self, raw_file: str):
-        return os.path.join(self.raw_path, "msms_" + os.path.splitext(raw_file)[0] + ".txt")
+        return os.path.join(self.raw_path, "msms_" + os.path.splitext(raw_file)[0] + ".prosit")
     
     def _get_split_prosit_path(self, raw_file: str):
         return os.path.join(self.raw_path, 'prosit_' + os.path.splitext(raw_file)[0] + '.tab')
@@ -64,15 +69,21 @@ class ReScore(CalculateFeatures):
             
             split_msms = self._get_split_msms_path(raw_file)
             logger.info(f"Creating split msms.txt file {split_msms}")
+            print(df_search_split['PEPTIDE_LENGTH'].dtype)
+            df_search_split = df_search_split[(df_search_split['PEPTIDE_LENGTH'] <= 30)]
+            df_search_split = df_search_split[(~df_search_split['MODIFIED_SEQUENCE'].str.contains('\(ac\)'))]
+            df_search_split = df_search_split[(~df_search_split['SEQUENCE'].str.contains('U'))]
+            df_search_split = df_search_split[df_search_split['PRECURSOR_CHARGE'] <= 6]
+            df_search_split = df_search_split[df_search_split['PEPTIDE_LENGTH'] >= 7]
             df_search_split.to_csv(split_msms, sep='\t',index=False)    
         
         open(split_msms_done_file, 'w').close()
     
-    def calculate_features(self, num_threads = 6):
+    def calculate_features(self, num_threads = 5):
         if num_threads > 1:
+            tracemalloc.start()
             from . import multiprocessing_pool as pool
             processingPool = pool.MyPool(processes = num_threads)
-
         for raw_file in self.raw_files:
             raw_file_path = os.path.join(self.raw_path, raw_file)
             percolator_input_path = self._get_split_prosit_path(raw_file)
@@ -80,9 +91,9 @@ class ReScore(CalculateFeatures):
             
             if not os.path.isfile(percolator_input_path):
                 if num_threads > 1:
-                    processingPool.applyAsync(calculate_features_single, (raw_file_path, split_msms_path, percolator_input_path))
+                    processingPool.applyAsync(self.calculate_features_single, (raw_file_path, split_msms_path, percolator_input_path))
                 else:
-                    calculate_features_single(raw_file_path, split_msms_path, percolator_input_path)
+                    self.calculate_features_single(raw_file_path, split_msms_path, percolator_input_path)
             else:
                 logger.info(f"Found percolator input file {percolator_input_path} for raw file {raw_file}, skipping feature calculation.")
             
@@ -105,8 +116,7 @@ class ReScore(CalculateFeatures):
                           test_fdr: float = 0.01, train_fdr: float = 0.01):
         """
         Use percolator to re-score library.
-        """
-        
+        """        
         if not os.path.isdir(output_path):
             os.makedirs(output_path)
         
@@ -131,4 +141,25 @@ class ReScore(CalculateFeatures):
                           {percolator_input_file} 2> {log_file}"
         logger.info(f"Starting percolator with command {cmd}")
         subprocess.run(cmd, shell=True, check=True)
+        pass
+
+
+
+if __name__ == "main":
+    ce_cal = CeCalibration(search_path = "D:/Compmass/workDir/HCD_OT/msms.txt",
+                          raw_path = "D:/Compmass/workDir/HCD_OT/190416_FPTMT_MS3_HCDOT_R1.mzml")
+    df_search = ce_cal._load_search()
+    grouped_search = df_search.groupby('RAW_FILE')
+    raw_files = grouped_search.groups.keys()
+    re_score_raw = {}
+    for raw_file in raw_files:
+        re_score_raw[raw_file] = ReScore(search_path="D:/Compmass/workDir/HCD_OT/msms.txt",
+                                             raw_path="D:/Compmass/workDir/HCD_OT/" + raw_file + ".mzml")
+        msms_raw = grouped_search.get_group(raw_file)
+        msms_raw = msms_raw[~msms_raw['SEQUENCE'].str.contains('U')]
+        msms_raw = msms_raw[msms_raw['PRECURSOR_CHARGE']<=6]
+        re_score_raw[raw_file].predict_with_aligned_ce(msms_raw)
+        re_score_raw[raw_file].percolator = re_score_raw[raw_file].gen_perc_metrics()
+        re_score_raw[raw_file].percolator = re_score_raw.percolator[ReScore.align_percolator_cols()]
+        re_score_raw[raw_file].percolator.to_csv('prosit_' + raw_file +'.tab', sep='\t',index=False)
 
