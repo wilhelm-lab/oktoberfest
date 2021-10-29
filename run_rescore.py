85369c9a96682489902e2af3118da4a0434ef216
Wassim Gabriel <ga79yaz@mytum.de>
2021-10-29 14:16:02 +0200

    Added a script to run rescore needs further improvments for generalization

---
+++ run_rescore.py
@@ -0,0 +1,16 @@
+from oktoberfest.ce_calibration import CeCalibration
+from oktoberfest.re_score import ReScore
+import datetime
+re_score_raw = ReScore(search_path="/media/kusterlab/internal_projects/active/microFlAIMS/06_Fractionation_LF/txt_100per/msms.txt",
+                       raw_path="/media/kusterlab/internal_projects/active/microFlAIMS/06_Fractionation_LF/",
+                       out_path="/media/kusterlab/internal_projects/active/microFlAIMS/06_Fractionation_LF/new_oktoberfest/")
+
+re_score_raw.get_raw_files()
+re_score_raw.split_msms()
+re_score_raw.calculate_features()
+re_score_raw.merge_input('Prosit')
+#re_score_raw.merge_input('Andromeda')
+
+re_score_raw.rescore_with_perc('Prosit')
+#re_score_raw.rescore_with_perc('Andromeda')
+
