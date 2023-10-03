d445834efff3fb9872ebfd7dcedf8c41e34da867
Mario Picciani <mario.picciani@tum.de>
2023-10-03 16:32:09 +0200

    corrected port of publikc koina server

--- tests/unit_tests/test_predictions.py
+++ tests/unit_tests/test_predictions.py
@@ -15,7 +15,7 @@ class TestTMTProsit(unittest.TestCase):
         library = Spectra.from_csv(Path(__file__).parent / "data" / "predictions" / "library_input.csv")
         grpc_predict(
             library=library,
-            url="koina.proteomicsdb.org:8500",
+            url="koina.proteomicsdb.org:443",
             intensity_model="Prosit_2020_intensity_TMT",
             irt_model="Prosit_2020_irt_TMT",
             ssl=True,
