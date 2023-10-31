8272e3c4cae44b74cfdf79a391d598e947a0563a
Mario Picciani <mario.picciani@tum.de>
2023-10-31 11:33:16 +0100

    fixed patch import issue

--- tests/unit_tests/test_runner.py
+++ tests/unit_tests/test_runner.py
@@ -1,5 +1,6 @@
 import unittest
 from pathlib import Path
+from unittest.mock import patch
 
 from oktoberfest.__main__ import main
 
@@ -10,5 +11,5 @@ class TestRunner(unittest.TestCase):
     def test_speclib_digest(self):
         """Test the runner for a spectral library generation with a fasta digest."""
         config_path = Path(__file__).parent / "configs" / "spectral_library_with_digest.json"
-        with unittest.mock.patch("sys.argv", ["oktoberfest", f"--config_path={config_path}"]):
+        with patch("sys.argv", ["oktoberfest", f"--config_path={config_path}"]):
             main()
