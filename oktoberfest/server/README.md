6718e9792f96d02c9132774235011193fad1097b
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2024-01-26 10:10:41 +0000

    Provide some documentation for the flask server.

---
+++ oktoberfest/server/README.md
@@ -0,0 +1,4 @@
+# Oktoberfest Server
+
+To start the flask server run `gunicorn oktoberfest.server.__main__:app`.
+TO start a development server where CORS is enabled run `python -m oktoberfest.server`
--- oktoberfest/server/api/v1/endpoints.py
+++ oktoberfest/server/api/v1/endpoints.py
@@ -13,6 +13,19 @@ v1_blueprint = Blueprint("v1", __name__)
 
 
 def calculate_hash(hashInput):
+    """
+    Calculate a SHA256 hash of the input string, salted with a global salt value.
+
+    This function takes an input string, encodes it to bytes using UTF-8 encoding, and then 
+    calculates a SHA256 hash of the encoded string. The global salt value is also encoded to 
+    bytes and added to the hash. The resulting hash is then truncated to the size specified 
+    by the global HASHSIZE value.
+
+    :param hashInput: The input string to hash.
+    :type hashInput: str
+    :return: The calculated hash.
+    :rtype: str
+    """
     m = sha256()
     m.update(hashInput.encode("utf-8"))
     m.update(SALT.encode("utf-8"))
@@ -20,6 +33,17 @@ def calculate_hash(hashInput):
 
 
 def dbGetStatus(taskId):
+    """
+    Get the status of a task from the database.
+
+    This function connects to the database and retrieves the status of the job with the given taskId 
+    from the JOBS table. If no status is found, None is returned.
+
+    :param taskId: The ID of the task.
+    :type taskId: str
+    :return: The status of the task, or None if no status is found.
+    :rtype: str or None
+    """
     with sqlite3.connect(DB_FILE) as conn:
         c = conn.cursor()
         c.execute("SELECT STATUS FROM JOBS WHERE TASK_ID = ?", [taskId])
@@ -27,6 +51,17 @@ def dbGetStatus(taskId):
 
 
 def dbFilesUploaded(taskId):
+    """
+    Update the database to indicate that files have been uploaded for a task.
+
+    This function connects to the database, starts an exclusive transaction, and inserts a new 
+    record into the JOBS table with the status 'UPLOADED'. The ID of the new record is one more 
+    than the maximum ID currently in the JOBS table. The current date and time is also recorded. 
+    If the database update is successful, the transaction is committed.
+
+    :param taskId: The ID of the task.
+    :type taskId: str
+    """
     with sqlite3.connect(DB_FILE) as conn:
         c = conn.cursor()
         c.execute("BEGIN EXCLUSIVE")
@@ -40,6 +75,19 @@ def dbFilesUploaded(taskId):
 
 
 def dbSubmitJob(taskId, config):
+    """
+    Update the database to indicate that a job has been submitted and is ready to be executed.
+
+    This function connects to the database, starts an exclusive transaction, and updates the 
+    status of the job with the given taskId to 'PENDING'. It also inserts the job configuration 
+    into the CONFIGS table. If the job submission is successful, the transaction is committed. 
+    If the job submission fails, the transaction is rolled back.
+
+    :param taskId: The ID of the task.
+    :type taskId: str
+    :param config: The configuration for the job.
+    :type config: str
+    """
     with sqlite3.connect(DB_FILE) as conn:
         c = conn.cursor()
         c.execute("BEGIN EXCLUSIVE")
@@ -52,11 +100,33 @@ def dbSubmitJob(taskId, config):
 
 @v1_blueprint.route("/status")
 def status():
+    """
+    Endpoint to check if the server is running.
+    """
     return {"status": 200}
 
+@v1_blueprint.route("/runningJobs")
+def runningJobs():
+    return {"count": 5}
 
 @v1_blueprint.route("/submitJob", methods=["POST"])
 def submitJob():
+    """
+    Endpoint to submit a job.
+
+    This endpoint accepts a JSON object in the request body containing the job configuration. 
+    The 'hashInput' parameter in the request args is used to calculate the taskId. The job 
+    configuration and taskId are then used to submit the job to the database. If the job 
+    submission is successful, the config written to the database is returned. If the job submission fails, 
+    an error message is returned.
+
+    :param hashInput: The input string used to calculate the taskId.
+    :type hashInput: str
+    :param config: The configuration for the job, passed in the request body.
+    :type config: dict
+    :return: A dictionary containing a success or error message.
+    :rtype: dict
+    """
     taskId = calculate_hash(request.args.get("hashInput"))
     jobConfig = request.json
     jobConfig["taskId"] = taskId
@@ -72,6 +142,24 @@ def submitJob():
 
 @v1_blueprint.route("/uploadFile", methods=["POST"])
 def uploadFile():
+    """
+    Endpoint to upload a file for a task. 
+
+    This endpoint accepts a file in the request body and two parameters in the request args: 
+    'hashInput' and 'taskId'. The 'hashInput' is used to calculate the taskId. If the 'taskId' 
+    parameter does not match the calculated taskId, it is assumed that this is the first file 
+    being uploaded for the task and a new directory is created for the task. If a directory 
+    already exists with the same taskId, a hash collision is assumed and an error message is returned.
+
+    :param hashInput: The input string used to calculate the taskId.
+    :type hashInput: str
+    :param taskId: The ID of the task. This should match the taskId calculated from the hashInput.
+    :type taskId: str
+    :param file: The file to be uploaded.
+    :type file: werkzeug.datastructures.FileStorage
+    :return: A dictionary containing the taskId.
+    :rtype: dict
+    """
     taskId = calculate_hash(request.args.get("hashInput"))
     folderPath = os.path.join(WORKDIR, taskId)
 
@@ -92,6 +180,18 @@ def uploadFile():
 
 @v1_blueprint.route("/jobStatus")
 def jobStatus():
+    """
+    Endpoint to get the status of a job.
+
+    This endpoint accepts a 'hashInput' parameter in the request args, which is used to calculate 
+    the taskId. The status of the job with the calculated taskId is then retrieved from the database. 
+    If no status is found, the status is set to 'UNKNOWN'.
+
+    :param hashInput: The input string used to calculate the taskId.
+    :type hashInput: str
+    :return: A dictionary containing the taskId and the status of the job.
+    :rtype: dict
+    """
     taskId = calculate_hash(request.args.get("hashInput"))
     resp = {"id": taskId}
     try:
@@ -103,6 +203,18 @@ def jobStatus():
 
 @v1_blueprint.route("/downloadResults")
 def downloadResults():
+    """
+    Endpoint to download the results of a job.
+
+    This endpoint accepts a 'hashInput' parameter in the request args, which is used to calculate 
+    the taskId. The results of the job with the calculated taskId are then retrieved from the 
+    file system. If no results are found, an error message is returned.
+
+    :param hashInput: The input string used to calculate the taskId.
+    :type hashInput: str
+    :return: A file containing the results of the job.
+    :rtype: werkzeug.datastructures.FileStorage
+    """
     taskId = calculate_hash(request.args.get("hashInput"))
     folder = os.path.abspath(os.path.join(WORKDIR, taskId))
     if os.path.isfile(os.path.join(folder, "results.zip")):
