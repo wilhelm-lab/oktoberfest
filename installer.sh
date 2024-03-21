01b81296529f51e1f16768cda5d520d24ab0be82
Mario Picciani <mario.picciani@tum.de>
2024-03-21 14:23:54 +0100

    bugfixing and making installer more stable

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 # === install mono, ThermoRawFileParser and percolator as root === #
 
-sudo apt -y update && sudo apt -y install mono-devel
+sudo apt -y update && sudo apt -y install mono-devel unzip
 
 wget https://github.com/percolator/percolator/releases/download/rel-3-06-01/percolator-v3-06-linux-amd64.deb -O /tmp/percolator.deb
 sudo dpkg -i /tmp/percolator.deb
@@ -16,6 +16,7 @@ rm /tmp/$ZIP
 
 # ============== user level conda setup installation ============= #
 
+INSTALLED_MINICONDA=0
 # Check if Anaconda is installed
 if command -v conda &> /dev/null; then
     CONDA_INSTALL_DIR=$(conda info --base)
@@ -23,8 +24,10 @@ else
     CONDA_INSTALL_DIR="$HOME/miniconda3/"
     echo "Miniconda not found. Installing Miniconda..."
     wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
-    /tmp/miniconda.sh -b -p "$CONDA_INSTALL_DIR"
+    bash /tmp/miniconda.sh -b -p "$CONDA_INSTALL_DIR"
     rm /tmp/miniconda.sh
+    conda init
+    INSTALLED_MINICONDA=1
 fi
 # Add Miniconda/Anaconda binary directory to PATH if not already present
 if [[ ":$PATH:" != *":$CONDA_INSTALL_DIR/bin:"* ]]; then
@@ -62,14 +65,41 @@ if [ $? -ne 0 ]; then
 fi
 # =========================== summary ============================ #
 
+perc_version=`percolator --help 2>&1 | head -n 1`
+if [ $? -ne 0 ]; then
+    echo "Percolator installation failed. Check logs."
+    exit 1
+fi
+
+mono_version=`mono --version | head -n 1`
+if [ $? -ne 0 ]; then
+    echo "Mono installation failed. Check logs."
+    exit 1
+fi
+
+thermo_version=`mono /opt/compomics/ThermoRawFileParser.exe --version`
+if [ $? -ne 0 ]; then
+    echo "ThermoRawFileParser installation failed. Check logs."
+    exit 1
+fi
+
+oktoberfest_version=`pip show oktoberfest | grep Version`
+if [ $? -ne 0 ]; then
+    echo "Oktoberfest installation failed. Check logs."
+    exit 1
+fi
 
 echo -e "\n"
-echo "percolator version: `percolator --help 2>&1 | head -n 1`"
-echo "mono version: `mono --version | head -n 1`"
-echo "ThermoRawFileParser version: `mono /opt/compomics/ThermoRawFileParser.exe --version`"
-echo "Oktoberfest version: `pip show oktoberfest | grep Version`"
+echo "percolator version: $perc_version"
+echo "mono version: $mono_version"
+echo "ThermoRawFileParser version: $thermo_version"
+echo "Oktoberfest version: $oktoberfest_version"
 
-echo -e "\nInstallation complete. To use oktoberfest, perform the following steps:"
+echo -e "\nInstallation complete."
+if [ $INSTALLED_MINICONDA -eq 1 ]; then
+    echo "Miniconda was installed. You need to restart your shell once before you can use oktoberfest."
+fi
+echo -e "\nTo use oktoberfest, perform the following steps:"
 echo -e "\t 1. activate your environment, using \"conda activate oktoberfest\""
 echo -e "\t 2. start jupyterlab using \"jupyter lab\" or run oktoberfest directly. Type \"python -m oktoberfest\" to get info."
 
