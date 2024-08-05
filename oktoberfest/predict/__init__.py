4e90290ea535429049b39413e8dd05baeaca219c
Julius Schlensok <julius@schlensok.org>
2024-08-05 23:55:31 +0000

    chore: expose DLomix & Koina interfaces through public prediction API

--- oktoberfest/predict/__init__.py
+++ oktoberfest/predict/__init__.py
@@ -1,5 +1,7 @@
 """Init predict."""
 
+from .dlomix import DLomix, create_dlomix_dataset, refine_intensity_predictor
+from .koina import Koina
 from .predictor import Predictor
 
-__all__ = ["Predictor"]
+__all__ = ["create_dlomix_dataset", "DLomix", "Koina", "Predictor", "refine_intensity_predictor"]
