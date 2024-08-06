bddb31ea5b090216be15ffccbcaa56c01c0ebba0
Julius Schlensok <julius@schlensok.org>
2024-08-06 17:01:30 +0000

    perf(DLomix): reduce batch size to not blow up GPU

--- oktoberfest/utils/config.py
+++ oktoberfest/utils/config.py
@@ -284,10 +284,11 @@ class Config:
         """Get output format from the config file."""
         return self.spec_lib_options.get("collisionEnergy", 30)
 
+    # TODO separate this for DL & non-DL
     @property
     def batch_size(self) -> int:
         """Get output format from the config file."""
-        return self.spec_lib_options.get("batchsize", 10000)
+        return self.spec_lib_options.get("batchsize", 1024)
 
     @property
     def min_intensity(self) -> float:
