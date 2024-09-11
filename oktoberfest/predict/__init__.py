b8ecf3fd7bfb0890608efdc474ad85d03626665c
Julius Schlensok <julius@schlensok.org>
2024-09-11 22:28:23 +0000

    fix(predict): only import dlomix module if dlomix is installed
    https://github.com/wilhelm-lab/oktoberfest/pull/250#discussion_r1743597369

--- oktoberfest/predict/__init__.py
+++ oktoberfest/predict/__init__.py
@@ -1,7 +1,8 @@
 """Init predict."""
 
-from .dlomix import DLomix, create_dlomix_dataset, refine_intensity_predictor
 from .koina import Koina
 from .predictor import Predictor
+if importlib.util.find_spec("dlomix"):
+    from .dlomix import DLomix, create_dlomix_dataset, refine_intensity_predictor
 
 __all__ = ["create_dlomix_dataset", "DLomix", "Koina", "Predictor", "refine_intensity_predictor"]
