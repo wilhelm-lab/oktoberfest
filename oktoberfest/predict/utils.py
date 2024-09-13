0320f10affcd7558f9cedb5c48b8985cadfb653e
Julius Schlensok <julius@schlensok.org>
2024-09-13 09:38:45 +0000

    style: pre-commit

--- oktoberfest/predict/utils.py
+++ oktoberfest/predict/utils.py
@@ -2,17 +2,20 @@ import numpy as np
 
 from oktoberfest.data import Spectra
 
+
 class ZeroPredictor:
     """Predictor implementation that returns unmodified peptide properties."""
+
     def __init__(self):
+        """Nothing to do here."""
         pass
 
     def predict(self, data: Spectra) -> dict[str, np.ndarray]:
         """Return unmodified peptide properties.
 
-       :param data: spectral library whose features will be returned
+        :param data: spectral library whose features will be returned
 
-       :return: a dictionary containing original peptide features in the same format as predictions
+        :return: a dictionary containing original peptide features in the same format as predictions
 
         """
         # TODO implement for intensity
