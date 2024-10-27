80acbc1290d7dd16af8414f09769a68eeebecc9a
Mostafa Kalhor <mostafa.kalhor@tum.de>
2024-10-27 11:21:27 +0000

    added runner unit test and required data for xl

--- tests/unit_tests/test_runner.py
+++ tests/unit_tests/test_runner.py
@@ -5,6 +5,8 @@ from unittest.mock import patch
 
 from oktoberfest.__main__ import main
 from oktoberfest.utils import Config
+import numpy as np
+import pandas as pd
 
 
 class TestRunner(unittest.TestCase):
@@ -19,3 +21,19 @@ class TestRunner(unittest.TestCase):
         config = Config()
         config.read(config_path)
         shutil.rmtree(config.output)
+
+    def test_rescoring_xl(self):
+        """Test the runner for a rescoring run with crosslinking."""
+        config_path = Path(__file__).parent / "configs" / "rescoring_cleavable_xl.json"
+        with patch("sys.argv", ["oktoberfest", f"--config_path={config_path}"]):
+            main()
+
+        #expected_perc_tab_file = pd.read_csv(Path(__file__).parent / "data" / "xl" / "cleavable" / "expected_outputs" / "expected_rescore.tab", sep="\t")
+        #created_perc_tab_file = pd.read_csv(Path(__file__).parent / "data" / "xl" / "cleavable" / "out" / "results" / "percolator" / "rescore.tab", sep="\t")
+        #np.testing.assert_almost_equal(expected_perc_tab_file.values, created_perc_tab_file.values)
+        #pd.testing.assert_frame_equal(expected_perc_tab_file, created_perc_tab_file)
+
+        config = Config()
+        config.read(config_path)
+        shutil.rmtree(Path(__file__).parent / "data" / "xl" / "cleavable" / "out")
+        
