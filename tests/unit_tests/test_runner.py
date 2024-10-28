197e0c5053900ac77da88f777af42c3c8d3ae0b9
Mostafa Kalhor <mostafa.kalhor@tum.de>
2024-10-28 16:21:51 +0000

    fixed test_runner.py

--- tests/unit_tests/test_runner.py
+++ tests/unit_tests/test_runner.py
@@ -2,7 +2,8 @@ import shutil
 import unittest
 from pathlib import Path
 from unittest.mock import patch
-
+import oktoberfest as ok
+from oktoberfest.runner import _preprocess, _ce_calib, _calculate_features, prepare_rescore_xl_psm_level
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
@@ -26,15 +27,52 @@ class TestRunner(unittest.TestCase):
     def test_rescoring_xl(self):
         """Test the runner for a rescoring run with crosslinking."""
         config_path = Path(__file__).parent / "configs" / "rescoring_cleavable_xl.json"
-        with patch("sys.argv", ["oktoberfest", f"--config_path={config_path}"]):
-            main()
+        #with patch("sys.argv", ["oktoberfest", f"--config_path={config_path}"]):
+        config = Config()
+        config.read(config_path)
+        config.check()
+
+        # load spectra file names
+        spectra_files = ok.pp.list_spectra(input_dir=config.spectra, input_format=config.spectra_type)
+
+        proc_dir = config.output / "proc"
+        proc_dir.mkdir(parents=True, exist_ok=True)
+
+        spectra_file = _preprocess(spectra_files, config)[0]
+        _ = _ce_calib(spectra_file, config)
+  
+        _calculate_features(spectra_file, config, xl=True)
+                
+        # prepare rescoring
+
+        fdr_dir = config.output / "results" / config.fdr_estimation_method
+        original_tab_files = [fdr_dir / spectra_file.with_suffix(".original.tab").name for spectra_file in spectra_files]
+        rescore_tab_files = [fdr_dir / spectra_file.with_suffix(".rescore.tab").name for spectra_file in spectra_files]
+            
+        ok.re.merge_input(tab_files=original_tab_files, output_file=fdr_dir / "original.tab")
+        ok.re.merge_input(tab_files=rescore_tab_files, output_file=fdr_dir / "rescore.tab")
+        
+
+        # prepare rescoring file
+        
+        rescore_features_path = fdr_dir / "rescore_features_csm.tab"
+        if not rescore_features_path.exists():
+            shutil.copy(fdr_dir / "rescore.tab", rescore_features_path)
+            input_psm_rescore = prepare_rescore_xl_psm_level(str(fdr_dir), "rescore")
+            input_psm_rescore.to_csv(str(fdr_dir) + "/rescore.tab", sep="\t", index=None)
+
+        original_features_path = fdr_dir / "original_features_csm.tab"
+        if not original_features_path.exists():
+            shutil.copy(fdr_dir / "original.tab", original_features_path)
+            input_psm_original = prepare_rescore_xl_psm_level(str(fdr_dir), "original")
+            input_psm_original.to_csv(str(fdr_dir) + "/original.tab", sep="\t", index=None)
 
         expected_perc_tab_file = pd.read_csv(
             Path(__file__).parent / "data" / "xl" / "cleavable" / "expected_outputs" / "expected_rescore.tab", sep="\t"
         )
-
+        
         created_perc_tab_file = pd.read_csv(
-            Path(__file__).parent / "data" / "xl" / "cleavable" / "out" / "results" / "percolator" / "rescore.tab",
+             fdr_dir/ "rescore.tab",
             sep="\t",
         )
 
